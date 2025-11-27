211dd5224410eeffe8ea64a65d7a440d6e3b3b19
aditrastogics24-glitch <aditrastogi.cs24@bmsce.ac.in>
2025-11-27 10:39:38 +0530

    Add files via upload

--- Supplier Database.sql
+++ Supplier Database.sql
@@ -19,26 +19,26 @@ primary key(sid,pid),
 foreign key(sid) references supplier(sid),
 foreign key(pid) references parts(pid));
 
-insert into supplier values(10001,'Acme Widget','Bangalore');
-insert into supplier values(10002,'Johns','Kolkata');
-insert into supplier values(10003,'Vimal','Mumbai');
-insert into supplier values(10004,'Reliance','Delhi');
+insert into supplier values(10001,'Acme Widget','Bangalore'),
+  (10002,'Johns','Kolkata'),
+  (10003,'Vimal','Mumbai'),
+  (10004,'Reliance','Delhi');
 
-insert into parts values(20001,'Book','Red');
-insert into parts values(20002,'Pen','Red');
-insert into parts values(20003,'Pencil','Green');
-insert into parts values(20004,'Mobile','Green');
-insert into parts values(20005,'Charger','Black');
+insert into parts values(20001,'Book','Red'),
+(20002,'Pen','Red'),
+(20003,'Pencil','Green'),
+(20004,'Mobile','Green'),
+(20005,'Charger','Black');
 
-insert into catalog values(10001,20001,10);
-insert into catalog values(10001,20002,10);
-insert into catalog values(10001,20003,30);
-insert into catalog values(10001,20004,10);
-insert into catalog values(10001,20005,10);
-insert into catalog values(10002,20001,10);
-insert into catalog values(10002,20002,20);
-insert into catalog values(10003,20003,30);
-insert into catalog values(10004,20003,40);
+insert into catalog values(10001,20001,10),
+(10001,20002,10),
+(10001,20003,30),
+(10001,20004,10),
+(10001,20005,10),
+ (10002,20001,10),
+(10002,20002,20),
+(10003,20003,30),
+(10004,20003,40);
 
 select * from supplier;
 select * from parts;
@@ -52,12 +52,9 @@ NOT EXISTS(select p.pid from parts p
 where NOT EXISTS(select c1.sid from
 catalog c1 where c1.sid = c.sid and c1.pid = c.pid));
 
-select distinct s.sname from catalog c,
-supplier s where c.sid = s.sid and
-NOT EXISTS(select p.pid from parts p
-where p.color = 'Red' and NOT EXISTS
-(select c1.sid from catalog c1 where
-c1.sid and c1.pid = p.pid and p.color = 'Red'));
+select sname from supplier
+where not exists(select pid from parts where color='Red' and 
+pid not in (select pid from catalog where catalog.sid=supplier.sid));
 
 select p.pname from parts p, catalog c, supplier s
 where p.pid = c.pid and c.sid = s.sid and
@@ -67,7 +64,11 @@ where p.pid = c1.pid and c1.sid = s1.sid
 and s1.sname != 'Acme Widget');
 
 
-select p.pid, s.sname from parts p,
-supplier s, catalog c where c.pid = p.pid
-and c.sid = s.sid and c.cost = (select max(c1.cost)
-from catalog c1 where c1.pid = p.pid);
+select distinct c.sid from catalog c
+where c.cost > (select AVG(c1.cost)
+from catalog c1 where c1.pid = c.pid);
+
+select p.pname, p.pid, s.sname from parts p
+join catalog c on p.pid=c.pid
+join supplier s on c.sid=s.sid
+where (p.pid, c.cost) in (select pid, max(cost) from catalog group by pid);
